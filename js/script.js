a3e879deacc402dcc78df6788874f8a9d9d657cc
BeatrizCSSantos <beatrizcs96@hotmail.com>
2025-10-28 20:56:57 -0300

    feat: adição do script do projeto

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('input', (e) => {
+  const { id, value } = e.target;
+
+  if (id === 'cpf') {
+    e.target.value = value
+      .replace(/\D/g, '')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+  }
+
+  if (id === 'telefone') {
+    e.target.value = value
+      .replace(/\D/g, '')
+      .replace(/(\d{2})(\d)/, '($1) $2')
+      .replace(/(\d{5})(\d{4})$/, '$1-$2');
+  }
+
+  if (id === 'cep') {
+    e.target.value = value.replace(/\D/g, '').replace(/(\d{5})(\d)/, '$1-$2');
+  }
+});
